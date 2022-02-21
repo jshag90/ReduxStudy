a8d2562514751a3b5b5b19a147cbbdb06dcab8db
SeonHakJi <jshag90@naver.com>
2022-02-21 23:55:35 +0900

    [Add] middleware에 thunkMiddleware 추가 비동기 로그인 요청

--- 2.redux/actions/user.js
+++ 2.redux/actions/user.js
@@ -1,10 +1,51 @@
 //action
 const logIn = (data) => {
+  //async aciton creator
+  return (dispatch, getState) => {
+    dispatch(logInRequest(data));
+    try {
+      setTimeout(() => {
+        dispatch(
+          logInSuccess({
+            userId: 1,
+            nickname: "seonhak",
+          })
+        );
+      }, 2000);
+    } catch (e) {
+      dispatch(logInFailure(e));
+    }
+  };
+};
+
+const logInRequest = (data) => {
   return {
-    type: "LOG_IN",
+    type: "LOG_IN_REQUEST",
     data,
   };
 };
+
+const logInSuccess = (data) => {
+  return {
+    type: "LOG_IN_SUCCESS",
+    data,
+  };
+};
+
+const logInFailure = (data) => {
+  return {
+    type: "LOG_IN_FAILURE",
+    data,
+  };
+};
+
+// const logIn = (data) => {
+//sync action creator
+//   return {
+//     type: "LOG_IN",
+//     data,
+//   };
+// };
 
 const logOut = (data) => {
   return {
