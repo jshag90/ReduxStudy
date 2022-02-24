9953f56bac94fd9261ad45820a788947c1a0fa32
SeonHakJi <jshag90@naver.com>
2022-02-24 23:59:11 +0900

    [Add] 4.react-redux-immer

---
+++ 4.react-redux-immer/App.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { connect } from "react-redux";
+const { logIn, logOut } = require("./actions/user");
+
+class App {
+  onClick = () => {
+    this.props.dispatchLogIn({ id: "zerocho", password: "비밀번호" });
+  };
+
+  onLogout = () => {
+    this.props.dispatchLogOut();
+  };
+
+  render() {
+    const { user } = this.props;
+
+    return (
+      <div>
+        {user.isLoggingIn ? (
+          <div>로그인 중</div>
+        ) : user.data ? (
+          <div>{user.data.nickname}</div>
+        ) : (
+          "로그인 해주세요."
+        )}
+        {!user.data ? (
+          <button onClick={this.onClick}>로그인</button>
+        ) : (
+          <button onClick={this.onLogout}>로그아웃</button>
+        )}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  user: state.user,
+  posts: state.posts,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  dispatchLogIn: (data) => dispatch(logIn(data)),
+  dispatchLogOut: () => dispatch(logOut()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ 4.react-redux-immer/reducers/user.js
@@ -0,0 +1,24 @@
+const initialState = {
+  isLoggingIn: true,
+  data: null,
+};
+
+const userReducer = (prevState = initialState, action) => {
+  // 새로운 state 만들어주기
+  switch (action.type) {
+    case "LOG_IN":
+      return {
+        ...prevState,
+        data: action.data,
+      };
+    case "LOG_OUT":
+      return {
+        ...prevState,
+        data: null,
+      };
+    default:
+      return prevState;
+  }
+};
+
+module.exports = userReducer;
